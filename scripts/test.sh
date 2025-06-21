9de6c35c9aa03a5716a756acee731bdd0a533cf0
Benjamin Fischer <benjamin.fischer@evolutics.info>
2025-06-21 11:58:20 +0200

    Test Rust code

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,3 +5,12 @@ set -o errexit -o nounset -o pipefail
 cd -- "$(dirname -- "$0")/.."
 
 travel-kit
+
+rustup component add rustfmt
+cargo fmt --all -- --check
+
+rustup component add clippy
+cargo clippy --all-features --all-targets -- --deny warnings
+
+cargo check
+cargo test
