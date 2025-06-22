41b873a8b8d388b619aa2b28bf048d73a54a63b4
Benjamin Fischer <benjamin.fischer@evolutics.info>
2025-06-22 16:53:18 +0200

    Develop proof of concept to apply Compose to Kubernetes using Kompose
    See also: https://github.com/kube-rs/kube/blob/main/examples/kubectl.rs

--- README.md
+++ README.md
@@ -5,6 +5,16 @@
 ```
 minikube start
 tilt up
+
 kubectl apply --filename=manifests/object.yaml
-kubectl describe configmap/foo
+kubectl describe service/greet
+minikube ssh -- \
+  curl "$(kubectl get service/greet --template='{{.spec.clusterIP}}')":8080
 ```
+
+## TODO
+
+- Clean up objects, e.g., when 1 of 2 services is dropped from Compose config.
+- Handle errors, e.g., from `unwrap`.
+- Provide Docker plugin that essentially does
+  `docker compose config --format json "$@" | kubectl apply --filename -`.
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,36 @@
 use futures::StreamExt;
-use k8s_openapi::api::core::v1;
 use kube::api;
-use kube::api::Resource;
+use kube::api::ResourceExt;
+use kube::core;
+use kube::discovery;
 use kube::runtime::controller;
 use kube::runtime::watcher;
-use std::collections;
+use serde::Deserialize;
+use std::io::Write;
+use std::process;
 use std::sync;
+use std::thread;
 use tokio::time;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
-    let client = kube::Client::try_default().await?;
 
-    let config_map_generators = api::Api::<ConfigMapGenerator>::all(client.clone());
-    let config_maps = api::Api::<v1::ConfigMap>::all(client.clone());
+    let client = kube::Client::try_default().await?;
+    let discovery = discovery::Discovery::new(client.clone()).run().await?;
+
+    let compose_apps = api::Api::<ComposeApp>::all(client.clone());
 
     tracing::info!("Starting controller.");
 
-    controller::Controller::new(config_map_generators, watcher::Config::default())
-        .owns(config_maps, watcher::Config::default())
+    controller::Controller::new(compose_apps, watcher::Config::default())
+        // TODO: Watch owned objects.
         .shutdown_on_signal()
-        .run(reconcile, error_policy, sync::Arc::new(Context { client }))
+        .run(
+            reconcile,
+            error_policy,
+            sync::Arc::new(Context { client, discovery }),
+        )
         .for_each(|_| futures::future::ready(()))
         .await;
 
@@ -32,70 +41,112 @@ async fn main() -> anyhow::Result<()> {
 #[derive(
     kube::CustomResource, Debug, Clone, serde::Deserialize, serde::Serialize, schemars::JsonSchema,
 )]
-#[kube(group = "evolutics.info", version = "v1", kind = "ConfigMapGenerator")]
-#[kube(shortname = "cmg", namespaced)]
-struct ConfigMapGeneratorSpec {
+#[kube(group = "evolutics.info", version = "v1", kind = "ComposeApp")]
+#[kube(shortname = "compose", namespaced)]
+struct ComposeAppSpec {
     content: String,
 }
 
 struct Context {
     client: kube::Client,
+    discovery: discovery::Discovery,
 }
 
 #[derive(Debug, thiserror::Error)]
-enum Error {
-    #[error("Failed to create ConfigMap: {0}")]
-    ConfigMapCreationFailed(#[source] kube::Error),
-    #[error("MissingObjectKey: {0}")]
-    MissingObjectKey(&'static str),
-}
+enum Error {}
 
 async fn reconcile(
-    generator: sync::Arc<ConfigMapGenerator>,
+    compose_app: sync::Arc<ComposeApp>,
     context: sync::Arc<Context>,
 ) -> anyhow::Result<controller::Action, Error> {
-    let client = &context.client;
+    // TODO: Configure owner references.
 
-    let contents =
-        collections::BTreeMap::from([("content".to_string(), generator.spec.content.clone())]);
-    let owner_reference = generator.controller_owner_ref(&()).unwrap();
-    let config_map = v1::ConfigMap {
-        metadata: api::ObjectMeta {
-            name: generator.metadata.name.clone(),
-            owner_references: Some(vec![owner_reference]),
-            ..api::ObjectMeta::default()
-        },
-        data: Some(contents),
-        ..Default::default()
-    };
-    let config_map_api = api::Api::<v1::ConfigMap>::namespaced(
-        client.clone(),
-        generator
+    let documents = convert_with_kompose(&compose_app.spec.content).unwrap();
+    let document_count = documents.len();
+    let server_side_apply = api::PatchParams::apply("comkube").force();
+
+    for (index, document) in documents.into_iter().enumerate() {
+        let object = serde_yaml::from_value::<api::DynamicObject>(document).unwrap();
+        let gvk = core::GroupVersionKind::try_from(object.types.as_ref().unwrap()).unwrap();
+        let namespace = object
             .metadata
             .namespace
-            .as_ref()
-            .ok_or_else(|| Error::MissingObjectKey(".metadata.namespace"))?,
-    );
-    config_map_api
-        .patch(
-            config_map
-                .metadata
-                .name
-                .as_ref()
-                .ok_or_else(|| Error::MissingObjectKey(".metadata.name"))?,
-            &api::PatchParams::apply("configmapgenerator.kube-rt.evolutics.info"),
-            &api::Patch::Apply(&config_map),
-        )
-        .await
-        .map_err(Error::ConfigMapCreationFailed)?;
+            .as_deref()
+            .or(compose_app.metadata.namespace.as_deref());
+        let name = object.name_any();
+
+        if let Some((resource, capabilities)) = context.discovery.resolve_gvk(&gvk) {
+            let api = dynamic_api(resource, capabilities, context.client.clone(), namespace);
+            tracing::trace!(
+                "Applying {kind}, document {number}/{document_count}:\n{pretty_object}",
+                kind = gvk.kind,
+                number = index + 1,
+                pretty_object = serde_yaml::to_string(&object).unwrap(),
+            );
+            let data = serde_json::to_value(&object).unwrap();
+            let _result = api
+                .patch(&name, &server_side_apply, &api::Patch::Apply(data))
+                .await
+                .unwrap();
+            tracing::info!("Applied {kind} {name}.", kind = gvk.kind);
+        } else {
+            tracing::warn!(
+                "Cannot apply document for unknown {group}/{version}/{kind}.",
+                group = gvk.group,
+                version = gvk.version,
+                kind = gvk.kind,
+            );
+        }
+    }
+
     Ok(controller::Action::requeue(time::Duration::from_secs(300)))
 }
 
 fn error_policy(
-    _object: sync::Arc<ConfigMapGenerator>,
+    _object: sync::Arc<ComposeApp>,
     error: &Error,
     _context: sync::Arc<Context>,
 ) -> controller::Action {
     tracing::warn!("Reconcile failed: {error}");
     controller::Action::requeue(time::Duration::from_secs(1))
+}
+
+fn convert_with_kompose(compose_config: &str) -> anyhow::Result<Vec<serde_yaml::Value>> {
+    let mut child = process::Command::new("./kompose")
+        .args(["--file", "-", "convert", "--stdout"])
+        .stdin(process::Stdio::piped())
+        .stdout(process::Stdio::piped())
+        .spawn()?;
+
+    let mut stdin = child.stdin.take().unwrap();
+    thread::scope(|scope| {
+        scope.spawn(move || {
+            stdin.write_all(compose_config.as_bytes()).unwrap();
+        });
+    });
+
+    let output = child.wait_with_output()?;
+    // TODO: Check exit status.
+    // TODO: Check stderr.
+
+    let documents = serde_yaml::Deserializer::from_slice(&output.stdout)
+        .map(serde_yaml::Value::deserialize)
+        .collect::<Result<_, _>>()?;
+    Ok(documents)
+    // TODO: Consider returning JSON as this would be simpler interface.
+}
+
+fn dynamic_api(
+    resource: discovery::ApiResource,
+    capabilities: discovery::ApiCapabilities,
+    client: kube::Client,
+    namespace: Option<&str>,
+) -> api::Api<api::DynamicObject> {
+    if capabilities.scope == discovery::Scope::Cluster {
+        api::Api::all_with(client, &resource)
+    } else if let Some(namespace) = namespace {
+        api::Api::namespaced_with(client, namespace, &resource)
+    } else {
+        api::Api::default_namespaced_with(client, &resource)
+    }
 }
