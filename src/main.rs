50304bf3f31273e2a9839a60b9138be13484c6a5
Benjamin Fischer <benjamin.fischer@evolutics.info>
2025-06-22 11:09:56 +0200

    Avoid unclear abbreviations

--- src/main.rs
+++ src/main.rs
@@ -28,23 +28,23 @@ struct ConfigMapGeneratorSpec {
 /// Controller triggers this whenever our main object or our children changed
 async fn reconcile(
     generator: sync::Arc<ConfigMapGenerator>,
-    ctx: sync::Arc<Data>,
+    context: sync::Arc<Data>,
 ) -> anyhow::Result<controller::Action, Error> {
-    let client = &ctx.client;
+    let client = &context.client;
 
     let mut contents = collections::BTreeMap::new();
     contents.insert("content".to_string(), generator.spec.content.clone());
-    let oref = generator.controller_owner_ref(&()).unwrap();
-    let cm = v1::ConfigMap {
+    let owner_reference = generator.controller_owner_ref(&()).unwrap();
+    let config_map = v1::ConfigMap {
         metadata: api::ObjectMeta {
             name: generator.metadata.name.clone(),
-            owner_references: Some(vec![oref]),
+            owner_references: Some(vec![owner_reference]),
             ..api::ObjectMeta::default()
         },
         data: Some(contents),
         ..Default::default()
     };
-    let cm_api = api::Api::<v1::ConfigMap>::namespaced(
+    let config_map_api = api::Api::<v1::ConfigMap>::namespaced(
         client.clone(),
         generator
             .metadata
@@ -52,14 +52,15 @@ async fn reconcile(
             .as_ref()
             .ok_or_else(|| Error::MissingObjectKey(".metadata.namespace"))?,
     );
-    cm_api
+    config_map_api
         .patch(
-            cm.metadata
+            config_map
+                .metadata
                 .name
                 .as_ref()
                 .ok_or_else(|| Error::MissingObjectKey(".metadata.name"))?,
             &api::PatchParams::apply("configmapgenerator.kube-rt.evolutics.info"),
-            &api::Patch::Apply(&cm),
+            &api::Patch::Apply(&config_map),
         )
         .await
         .map_err(Error::ConfigMapCreationFailed)?;
@@ -70,7 +71,7 @@ async fn reconcile(
 fn error_policy(
     _object: sync::Arc<ConfigMapGenerator>,
     _error: &Error,
-    _ctx: sync::Arc<Data>,
+    _context: sync::Arc<Data>,
 ) -> controller::Action {
     controller::Action::requeue(time::Duration::from_secs(1))
 }
@@ -85,23 +86,23 @@ async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
     let client = kube::Client::try_default().await?;
 
-    let cmgs = api::Api::<ConfigMapGenerator>::all(client.clone());
-    let cms = api::Api::<v1::ConfigMap>::all(client.clone());
+    let config_map_generators = api::Api::<ConfigMapGenerator>::all(client.clone());
+    let config_maps = api::Api::<v1::ConfigMap>::all(client.clone());
 
     tracing::info!("starting configmapgen-controller");
 
     // limit the controller to running a maximum of two concurrent reconciliations
     let config = controller::Config::default().concurrency(2);
 
-    controller::Controller::new(cmgs, watcher::Config::default())
-        .owns(cms, watcher::Config::default())
+    controller::Controller::new(config_map_generators, watcher::Config::default())
+        .owns(config_maps, watcher::Config::default())
         .with_config(config)
         .shutdown_on_signal()
         .run(reconcile, error_policy, sync::Arc::new(Data { client }))
-        .for_each(|res| async move {
-            match res {
-                Ok(o) => tracing::info!("reconciled {:?}", o),
-                Err(e) => tracing::warn!("reconcile failed: {}", e),
+        .for_each(|result| async move {
+            match result {
+                Ok(object_reference) => tracing::info!("reconciled {:?}", object_reference),
+                Err(error) => tracing::warn!("reconcile failed: {}", error),
             }
         })
         .await;
