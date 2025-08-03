38a8d43aab62e129b56d9d7f97311ea702ca54bb
Benjamin Fischer <benjamin.fischer@evolutics.info>
2025-08-03 10:09:13 +0200

    Log Kompose stderr, if any

--- src/kompose.rs
+++ src/kompose.rs
@@ -7,6 +7,7 @@ pub fn convert(compose_config: &str) -> anyhow::Result<Vec<serde_yaml::Value>> {
     let mut child = process::Command::new("kompose")
         .args(["--file", "-", "convert", "--stdout"])
         .env("COMPOSE_PROJECT_NAME", "dummy")
+        .stderr(process::Stdio::piped())
         .stdin(process::Stdio::piped())
         .stdout(process::Stdio::piped())
         .spawn()?;
@@ -19,8 +20,13 @@ pub fn convert(compose_config: &str) -> anyhow::Result<Vec<serde_yaml::Value>> {
     });
 
     let output = child.wait_with_output()?;
+
+    if !output.stderr.is_empty() {
+        let stderr = String::from_utf8(output.stderr)?;
+        tracing::debug!("Kompose stderr:\n{stderr}");
+    }
+
     // TODO: Check exit status.
-    // TODO: Check stderr.
 
     let documents = serde_yaml::Deserializer::from_slice(&output.stdout)
         .map(serde_yaml::Value::deserialize)
