fc371574631cb972bf2a1dce4b683a9e14e6e789
Benjamin Fischer <benjamin.fischer@evolutics.info>
2025-08-03 12:49:15 +0200

    Test Helm chart notes

--- chart/testing/expected_notes.md
+++ chart/testing/expected_notes.md
@@ -1,1 +1,2 @@
+NOTES:
 Thanks for using Comkube.
--- chart/testing/test.sh
+++ chart/testing/test.sh
@@ -5,3 +5,7 @@ set -o errexit -o nounset -o pipefail
 cd -- "$(dirname -- "$0")"
 
 helm lint --strict ..
+
+helm install my-release .. --dry-run \
+  | sed --quiet '/^NOTES:$/,$p' \
+  | diff --label 'Actual notes' --unified - expected_notes.md
