9f9ecb5337cbf71c9ca66c2cec791209b9e6b34b
Benjamin Fischer <benjamin.fischer@evolutics.info>
2025-08-16 17:52:55 +0200

    Label owned objects with manager tool

--- src/get_desired_state.rs
+++ src/get_desired_state.rs
@@ -5,7 +5,6 @@ use kube::api::ResourceExt;
 
 pub fn get(compose_app: &super::ComposeApplication) -> Vec<api::DynamicObject> {
     let owner_reference = compose_app.controller_owner_ref(&()).unwrap();
-    // TODO: Label owned objects with `app.kubernetes.io/managed-by=Comkube`.
 
     // TODO: Report Kompose errors via Compose app object instead of crashing.
     let documents = kompose::convert(&serde_json::to_string(&compose_app.spec).unwrap()).unwrap();
@@ -14,6 +13,9 @@ pub fn get(compose_app: &super::ComposeApplication) -> Vec<api::DynamicObject> {
         .into_iter()
         .map(|document| {
             let mut object = serde_yaml::from_value::<api::DynamicObject>(document).unwrap();
+            object
+                .labels_mut()
+                .insert("app.kubernetes.io/managed-by".into(), "Comkube".into());
             object.owner_references_mut().push(owner_reference.clone());
             object
         })
