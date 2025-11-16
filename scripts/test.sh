5c6306a502004b8183db68ef116972e3cbb5b250
Benjamin Fischer <benjamin.fischer@evolutics.info>
2025-11-16 11:54:20 +0100

    Add usage example, testing it end-to-end

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,11 +2,35 @@
 
 set -o errexit -o nounset -o pipefail
 
-cd -- "$(dirname -- "$0")/.."
+test_example() {
+  minikube start
 
-travel-kit
-golangci-lint run --fix
-go test ./...
+  local -r namespace="${RANDOM}"
+  kubectl create namespace "${namespace}"
 
-docker build --build-arg "kompose_version=${KOMPOSE_VERSION}" \
-  --tag ghcr.io/evolutics/comkube:dirty .
+  kubectl kustomize --enable-alpha-plugins example \
+    | kubectl --namespace="${namespace}" apply --filename -
+
+  kubectl --namespace="${namespace}" wait --for=condition=Available \
+    deployment/greet
+
+  minikube ssh -- curl --fail-with-body "$(kubectl --namespace="${namespace}" \
+    get service/greet --template='{{.spec.clusterIP}}')":8080
+
+  kubectl delete namespace "${namespace}"
+}
+
+main() {
+  cd -- "$(dirname -- "$0")/.."
+
+  travel-kit
+  golangci-lint run --fix
+  go test ./...
+
+  docker build --build-arg "kompose_version=${KOMPOSE_VERSION}" \
+    --tag ghcr.io/evolutics/comkube:dirty .
+
+  test_example
+}
+
+main "$@"
