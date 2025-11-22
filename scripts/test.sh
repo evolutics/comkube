33226ef852f3ed51f1012658ed9e61c4c9954f6c
Benjamin Fischer <benjamin.fischer@evolutics.info>
2025-11-22 12:47:26 +0100

    Do not repeat image reference

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,11 +4,9 @@ set -o errexit -o nounset -o pipefail
 
 test_image_labels() {
   local -r title="$(docker image inspect --format \
-    '{{index .Config.Labels "org.opencontainers.image.title"}}' \
-    ghcr.io/evolutics/comkube:dirty)"
+    '{{index .Config.Labels "org.opencontainers.image.title"}}' "$1")"
   local -r description="$(docker image inspect --format \
-    '{{index .Config.Labels "org.opencontainers.image.description"}}' \
-    ghcr.io/evolutics/comkube:dirty)"
+    '{{index .Config.Labels "org.opencontainers.image.description"}}' "$1")"
 
   if ! grep --ignore-case "^# ${title}: ${description}$" README.md; then
     echo 'Image labels are inconsistent with readme title.' >&2
@@ -17,8 +15,8 @@ test_image_labels() {
 }
 
 test_kompose_version_in_image_is_consistent_with_native_env() {
-  local -r image_version_line="$(docker run --entrypoint kompose --rm \
-    ghcr.io/evolutics/comkube:dirty version)"
+  local -r image_version_line="$(docker run --entrypoint kompose --rm "$1" \
+    version)"
   local -r native_version_line="$(kompose version)"
 
   local -r image_version="${image_version_line%% *}"
@@ -60,10 +58,11 @@ main() {
   golangci-lint run --fix
   go test ./...
 
-  docker build --load --tag ghcr.io/evolutics/comkube:dirty .
+  local -r image='ghcr.io/evolutics/comkube:dirty'
+  docker build --load --tag "${image}" .
 
-  test_image_labels
-  test_kompose_version_in_image_is_consistent_with_native_env
+  test_image_labels "${image}"
+  test_kompose_version_in_image_is_consistent_with_native_env "${image}"
   test_example
 }
 
