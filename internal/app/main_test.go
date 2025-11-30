c1bb734ca043238619ef034a378378181b2eb53d
Benjamin Fischer <benjamin.fischer@evolutics.info>
2025-11-30 10:55:33 +0100

    Support different indentation levels

--- internal/app/main_test.go
+++ internal/app/main_test.go
@@ -15,21 +15,27 @@ func TestReadmeHasUpToDateFiles(test *testing.T) {
 	}
 	readme := string(rawReadme)
 
-	for _, file := range []string{
-		"../../examples/basic/kustomization.yaml",
-		"../../examples/basic/my-k8s-compose-app.yaml",
+	for _, check := range []struct {
+		file        string
+		indentation int
+	}{
+		{"../../examples/basic/kustomization.yaml", 3},
+		{"../../examples/basic/my-k8s-compose-app.yaml", 3},
 	} {
-		contents, err := os.ReadFile(file)
+		contents, err := os.ReadFile(check.file)
 		if err != nil {
 			test.Fatal(err)
 		}
 		codeBlock := fmt.Sprintf("```yaml\n%s```", contents)
 
 		lineStart := regexp.MustCompile(`(^|\n)`)
-		indentedBlock := lineStart.ReplaceAllString(codeBlock, "$1   ")
+		indentedBlock := lineStart.ReplaceAllString(
+			codeBlock,
+			fmt.Sprintf("$1%v", strings.Repeat(" ", check.indentation)),
+		)
 
 		if !strings.Contains(readme, indentedBlock) {
-			test.Errorf("File not in readme: %v\n%v", file, indentedBlock)
+			test.Errorf("File not in readme: %v\n%v", check.file, indentedBlock)
 		}
 	}
 }
